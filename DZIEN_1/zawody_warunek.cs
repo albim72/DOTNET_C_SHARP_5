8f0634efc11e2c0e050356760e939bfbd757293e
albim72 <marcin.albiniak72@gmail.com>
2026-05-06 13:24:19 +0200

    Update zawody_warunek.cs

--- DZIEN_1/zawody_warunek.cs
+++ DZIEN_1/zawody_warunek.cs
@@ -37,6 +37,40 @@ namespace complex_warunek
             {
                 Console.WriteLine("You are not allowed to participate in the event.");
             }
+
+            Console.WriteLine("______________________________________");
+
+            Console.WriteLine("Analiza problemów z zamówieniem....... ");
+
+            int paymentStatus = 1; // 0 - unpaid, 1 - paid, 2 - refunded 
+            int stockAmount = 5;
+            bool customerBlocked = true;
+            bool expressDelivery = true;
+
+            if (customerBlocked)
+            {
+                Console.WriteLine("Zamówienie nie może być zrealizowane - klient zablokowany.");
+            }
+            else if (paymentStatus == 0)
+            {
+                Console.WriteLine("Zamówienie nie może być zrealizowane - płatność nie została dokonana.");
+            }
+            else if (paymentStatus == 2 || paymentStatus == 0)
+            {
+                Console.WriteLine("Zamówienie nie może być zrealizowane - płatność została zwrócona.");
+            }
+            else if (stockAmount <= 0)
+            {
+                Console.WriteLine("Zamówienie nie może być zrealizowane - brak towaru w magazynie.");
+            }
+            else if (expressDelivery && stockAmount < 3)
+            {
+                Console.WriteLine("Zamówienie nie może być zrealizowane - niewystarczająca ilość towaru dla ekspresowej dostawy.");
+            }
+            else
+            {
+                Console.WriteLine("Zamówienie może być zrealizowane.");
+            }
         }
     }
 }
