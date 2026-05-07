3961364f2592dc0f06fba887af7dbb4571a24809
albim72 <marcin.albiniak72@gmail.com>
2026-05-07 12:53:11 +0200

    Add Pies and Kot classes with sound methods

---
+++ DZIEN_2/OBIEKTOWOSC/ABSTRAKCJA/PiesKot.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace abstrakcja
+{
+   class Pies : Zwierze
+    {
+        public Pies(string imie) : base(imie)
+        {
+        }
+        public override void WydajDzwiek()
+        {
+            Console.WriteLine("Hau hau!");
+        }
+    }
+    class Kot : Zwierze
+    {
+        public Kot(string imie) : base(imie)
+        {
+        }
+        public override void WydajDzwiek()
+        {
+            Console.WriteLine("Miau miau!");
+        }
+    }
+}
