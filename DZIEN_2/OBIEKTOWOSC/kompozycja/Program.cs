f124a0760e2c19d6129171a9e04b0104d2f1f947
albim72 <marcin.albiniak72@gmail.com>
2026-05-07 14:17:33 +0200

    Create Program.cs

---
+++ DZIEN_2/OBIEKTOWOSC/kompozycja/Program.cs
@@ -0,0 +1,9 @@
+using Samochod_kompozycja;
+
+Samochod samochod = new Samochod("Toyota", "Corolla", 1600);
+
+samochod.Uruchom();
+samochod.Zatrzymaj();
+
+Console.WriteLine("Naciśnij dowolny klawisz, aby zakończyć...");
+
