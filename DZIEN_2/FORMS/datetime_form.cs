41ba56342ba1fff4ac85ede6d3ed742c08139209
albim72 <marcin.albiniak72@gmail.com>
2026-05-07 14:52:49 +0200

    Create datetime_form.cs

---
+++ DZIEN_2/FORMS/datetime_form.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Data_okno
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dateTimePicker1.Format = DateTimePickerFormat.Short;
+            dateTimePicker1.Value = DateTime.Today;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DateTime iDate;
+            iDate = dateTimePicker1.Value;
+            MessageBox.Show("Wybrana data: " + iDate.ToShortDateString());
+        }
+    }
+}
