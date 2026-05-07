0694cbf32d8424393f8b3bf28ce70ce0be511354
albim72 <marcin.albiniak72@gmail.com>
2026-05-07 11:28:46 +0200

    Update Program.cs

--- DZIEN_2/OBIEKTOWOSC/OSOBA_STUDENT/Program.cs
+++ DZIEN_2/OBIEKTOWOSC/OSOBA_STUDENT/Program.cs
@@ -1,1 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace osoba_student
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student1 = new Student();
+            student1.UstawDaneStudenta("Jan", "Kowalski", 20, "12345", "Informatyka");
+            student1.WyswietlDaneStudenta();
+
+            Console.WriteLine("Koniec programu.... ");
+
+        }
+    }
+}
 
